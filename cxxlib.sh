43c6dc9721c82ee8401a4cdec4f5e82d0634967c
ESeNonFossiIo <esenonfossiio@gmail.com>
2015-04-20 17:52:55 +0200

    some very small changes

--- cxxlib.sh
+++ cxxlib.sh
@@ -4,19 +4,25 @@
 # Last edit: 29-03-2015
 
 # It creates a complete filesystem for a c++ project.
-# $1 is the name of the project
-# $2 is the name of the first class (in lower case)
+# $AUTHOR is the author of the project
+# $PROJECT_NAME is the name of the project
+# $CLASS is the name of the first class (in lower case)
 
 # Creation of the name of the class
-CLASS=$2
-CLASS=`echo ${CLASS:0:1} | tr  '[a-z]' '[A-Z]'`${CLASS:1}
+echo 
+read -e -p "Author: ................. " AUTHOR
+read -e -p "Name of the project: .... " PROJECT_NAME
+read -e -p "Name of the class: ...... " CLASS
 
+CLASS=`echo -e ${CLASS:0:1} | tr '[a-z]' '[A-Z]'`${CLASS:1}
+DATE=`date "+date: %Y-%m-%d"` 
+TIME=`date "+time: %H:%M:%S"`
 # filesystem creation
-mkdir $1 $1/include $1/source $1/build
+mkdir $PROJECT_NAME $PROJECT_NAME/include $PROJECT_NAME/source $PROJECT_NAME/build
 
 
 # main file
-echo '/*
+echo -e "/*
  * Usage:
  * cd build
  * cmake ..
@@ -25,13 +31,15 @@ echo '/*
  *
  * Description
  *
- * Author: Marco Tezzele
- * Last edit: 00-00-201
+ * Author: $AUTHOR
+ * $DATE
+ * $TIME
  */
 
 
 #include <iostream>
 #include <cmath>
+#include <vector>
 #include <cstdlib>
 #include <time.h>
 #include <ctype.h>
@@ -39,7 +47,7 @@ echo '/*
 #include <sys/time.h>
 
 
-#include "'$2'.h"
+#include \"$CLASS.h\"
 
 /* Returns elapsed seconds past from the last call to timer rest */
 double cclock()
@@ -57,12 +65,12 @@ int main(int argc, char *argv[])
 
 	return 0;
 }
-' >> $1/source/main.cc
+" >> $PROJECT_NAME/source/main.cc
 
 
 # source file of the class
-echo '/*
- * Author: Marco Tezzele
+echo -e "/*
+ * Author: $AUTHOR
  * Last edit: 00-00-201
  */
 
@@ -70,43 +78,62 @@ echo '/*
 #include <vector>
 #include <cmath>
 
-#include "'$2'.h"
+#include \"$CLASS.h\"
 
-'$CLASS'::'$CLASS '()
+
+template <int dim, int spacedim>
+$CLASS<dim,spacedim>::$CLASS ()
 :
 (),
 ()
 {}
 
 
-'$CLASS'::~'$CLASS '()
+template <int dim, int spacedim>
+$CLASS<dim,spacedim>::~$CLASS ()
 {}
 
 
-void '$CLASS'::print(std::ostream &out) const
+template <int dim, int spacedim>
+void $CLASS<dim,spacedim>::print(std::ostream &out) const
 {
 	out << std::endl;
 
 	return;
-}' >> $1/source/$2.cc
+}
+
+
+// explicit instantiations
+template class $CLASS<1,1>;
+template class $CLASS<1,2>;
+template class $CLASS<2,2>;
+template class $CLASS<1,3>;
+template class $CLASS<2,3>;
+template class $CLASS<3,3>;
+" >> $PROJECT_NAME/source/$CLASS.cc
 
 
 # header file of the class
-echo '/*
- * Author: Marco Tezzele
+echo -e "/*
+ * Author: $AUTHOR
+ * $DATE
+ * $TIME
  * Last edit: 00-00-201
  */
 
-#ifndef __'$2'__
-#define __'$2'__
+#ifndef __$CLASS__
+#define __$CLASS__
 
 #include <iostream>
+#include <vector>
 
-class '$CLASS'
+
+template <int dim, int spacedim=dim>
+class $CLASS
 {
 public:
-	'$CLASS '();
-	~'$CLASS '();
+	$CLASS ();
+	~$CLASS ();
 	
 	void print(std::ostream &out = std::cout) const;
 
@@ -115,31 +142,31 @@ private:
 };
 
 #endif
-' >> $1/include/$2.h
+" >> $PROJECT_NAME/include/$CLASS.h
 
 
 # cmake file with MPI and OMP
-echo 'cmake_minimum_required(VERSION 2.8)
+echo -e 'cmake_minimum_required(VERSION 2.8)
 
-project ('$1')
+project ('$PROJECT_NAME')
 file(GLOB cc_files source/*.cc)
 include_directories(include)
-add_executable('$1' ${cc_files})
+add_executable('$PROJECT_NAME' ${cc_files})
 
 
 
 find_package(MPI REQUIRED)
 include_directories(${MPI_INCLUDE_PATH})
 
-target_link_libraries('$1' ${MPI_LIBRARIES})
+target_link_libraries('$PROJECT_NAME' ${MPI_LIBRARIES})
 
 if(MPI_COMPILE_FLAGS)
-  set_target_properties('$1' PROPERTIES
+  set_target_properties('$PROJECT_NAME' PROPERTIES
     COMPILE_FLAGS "${MPI_COMPILE_FLAGS}")
 endif()
 
 if(MPI_LINK_FLAGS)
-  set_target_properties('$1' PROPERTIES
+  set_target_properties('$PROJECT_NAME' PROPERTIES
     LINK_FLAGS "${MPI_LINK_FLAGS}")
 endif()
 
@@ -152,8 +179,8 @@ endif()
 
 
 add_custom_target(run
-    COMMAND '$1'
-    DEPENDS '$1'
+    COMMAND '$PROJECT_NAME'
+    DEPENDS '$PROJECT_NAME'
     WORKING_DIRECTORY ${CMAKE_PROJECT_DIR}
-)' >> $1/CMakeLists.txt
+)' >> $PROJECT_NAME/CMakeLists.txt
 
