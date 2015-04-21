ccb0fb0b891807d978b7cf2385c1d6cbf0a1c458
ESeNonFossiIo <esenonfossiio@gmail.com>
2015-04-21 19:21:59 +0200

    some new interesting flags! a,p,c

--- cxxlib.sh
+++ cxxlib.sh
@@ -11,11 +11,17 @@
 source "include/utilities.sh"
 source "include/cxx_project.sh"
 
+MAKE_PROJECT_CHECK=false
+
 if [ ! -d "log" ]; then
     mkdir "log"
 fi
 
-while getopts lL:mh name
+AUTHOR="author"
+PROJECT_NAME="project"
+CLASSES="classes"
+
+while getopts ":lL:mha:p:c:" name
 do
     case $name in  
     h)  echo -e "$BAR"
@@ -24,22 +30,47 @@ do
         echo -e "\t -l        : Create a symbolic link to cxxlib in /usr/local/bin"
         echo -e "\t -L [path] : Create a symbolic link to cxxlib in path"
         echo -e "\t -m        : Create a new project"
+        echo -e "\t -a [path] : Author of the project"
+        echo -e "\t -p [path] : Name of the project"
+        echo -e "\t -c [path] : Classes of the project"
+        echo -e " "
+        echo -e "\t ATTENTION!  Every argument has to be included in \"...\" "
         echo -e "$BAR"
-        echo -e "$BAR" ;;
+        echo -e "$BAR" 
+        ;;
     l)  USR_BIN_PATH="/usr/local/bin"
         echo -e "$BAR"
         echo -e " Creating a symbolic link to cxxlib in $USR_BIN_PATH"
         echo -e "$BAR"
-        sudo ln -s $PWD/cxxlib.sh $USR_BIN_PATH/cxxlib 2>> log/getops.log ;;
+        sudo ln -s $PWD/cxxlib.sh $USR_BIN_PATH/cxxlib 2>> log/getops.log 
+        ;;
     L)  USR_BIN_PATH="$OPTARG"
         echo -e "$BAR"
         echo -e " Creating a symbolic link to cxxlib in $USR_BIN_PATH"
         echo -e "$BAR"
-        sudo ln -s $PWD/cxxlib.sh $USR_BIN_PATH/cxxlib 2>> log/getops.log ;;
-    m)  make_project;;
-    *)  printf "Usage: %s: [-l] [-m] \n" $0
-        exit 2 ;;
-    ?)  printf "Usage: %s: [-l] [-m] \n" $0
-        exit 2 ;;
+        sudo ln -s $PWD/cxxlib.sh $USR_BIN_PATH/cxxlib 2>> log/getops.log 
+        ;;
+    a)  AUTHOR="$OPTARG"
+        echo -e "$AUTHOR"
+        ;;
+    p)  PROJECT_NAME="$OPTARG"
+        ;;
+    c)  CLASSES="$OPTARG"   
+        ;;
+    m)  MAKE_PROJECT_CHECK=true
+        ;;
+    # :)  printf "Usage: %s: [-l] [-m] \n" $0
+    #     exit 2 ;;
+    # \?)  printf "Usage: %s: [-l] [-m] \n" $0
+    #     exit 2 ;;
     esac
 done
+
+if [ "$MAKE_PROJECT_CHECK" == true ]
+then
+    echo -e "$BAR"
+    echo -e "  CREATING THE PROJECT"
+    echo -e "$BAR"
+    make_project "$AUTHOR" "$PROJECT_NAME" "$CLASSES"
+    print_report
+fi
