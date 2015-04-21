e0fcc254a78a55cb4bdef5fe0b6961bda6bb7437
ESeNonFossiIo <esenonfossiio@gmail.com>
2015-04-21 16:33:34 +0200

    gnuplot function

--- make_plot.sh
+++ make_plot.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 
-function plot()
-{
-	gnuplot -e "
-		set term png;
-		set output \"${output}\"; 
-		set ylabel \"${y_label}\";
-		set xlabel \"${x_label}\";
-		set xrange ${xxrange};
-		set yrange ${yyrange};
-		set format x \"%g\";
-		set title \"${title}\";
-		plot ${what} "
-}
-
+source "include/gnuplot.sh"
 
 file="newton.dat"
 y_label="sqrt(x)"
