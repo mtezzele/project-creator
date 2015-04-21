c746f5348b117bbf0c23ab74bb63ae158be49945
ESeNonFossiIo <esenonfossiio@gmail.com>
2015-04-21 23:28:04 +0200

    function to generate .conf

--- include/gnuplot.sh
+++ include/gnuplot.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-function plot()
+source "include/utilities.sh"
+
+function plot
 {
 	gnuplot -e "
 		set term pdf;
@@ -13,4 +15,102 @@ function plot()
 		set title \"${title}\";
 		plot ${what} "
 }
-export -f function
+export -f plot
+
+function generate_conf
+{
+	if [ ! -d "_conf" ]; 
+	then
+    	mkdir "_conf"
+	fi
+	
+	title " Auto tool to generate .conf file"
+	
+	echo -e " -> Name of the file to generate (.conf)"
+	read -p "   -> name: " FILE_NAME
+	echo -e "$BBAR"
+	FILE_NAME="./_conf/${FILE_NAME}.conf"
+	
+	echo -e " -> Chose which term do you prefer: \n\t 1) pdf [default] \n\t 2) png "
+	read -p "   -> your choice: " CHOICE
+	case "$CHOICE" in 
+		2 ) echo -e "set term png;" >> "${FILE_NAME}"
+			FILE_EXT="png"
+			;;
+		* ) echo -e "set term pdf;" >> "${FILE_NAME}"
+			FILE_EXT="pdf"
+			;;
+
+	esac
+	echo -e "$BBAR"
+	
+	echo -e " -> name of the file to generate"
+	read -p "   -> name: " OUTPUT_FILE_NAME
+	echo -e "set output \"${OUTPUT_FILE_NAME}.${FILE_EXT}\";" >> "${FILE_NAME}"
+	echo -e "$BBAR"
+	
+	echo -e " -> xrange [Y]es or [n]o  "
+	read -p "   -> your choice: " CHOICE
+	case "$CHOICE" in 
+		n )
+			;;
+		* ) echo -e " -> write the range (es. [-1.0:10.5])  "
+			read -p "   -> your choice: " RANGE 
+			echo -e "set xrange ${RANGE};" >> "${FILE_NAME}"
+			;;
+
+	esac
+	echo -e "$BBAR"
+	
+	echo -e " -> yrange [Y]es or [n]o  "
+	read -p "   -> your choice: " CHOICE
+	case "$CHOICE" in 
+		n ) 	
+			;;
+		* ) echo -e " -> write the range (es. [-1.0:10.5])  "
+			read -p "   -> your choice: " RANGE 
+			echo -e "set yrange ${RANGE};" >> "${FILE_NAME}"
+			;;
+
+	esac
+	echo -e "$BBAR"
+	
+	echo -e " -> xlabel [Y]es or [n]o  "
+	read -p "   -> your choice: " CHOICE
+	case "$CHOICE" in 
+		n ) 	
+			;;
+		* ) echo -e " -> write the title  "
+			read -p "   -> your choice: " LABEL 
+			echo -e "set xlabel \"${LABEL}\";" >> "${FILE_NAME}"
+			;;
+	esac
+	echo -e "$BBAR"
+	
+	echo -e " -> ylabel [Y]es or [n]o  "
+	read -p "   -> your choice: " CHOICE
+	case "$CHOICE" in 
+		n ) 	
+			;;
+		* ) echo -e " -> write the title  "
+			read -p "   -> your choice: " LABEL 
+			echo -e "set ylabel \"${LABEL}\";" >> "${FILE_NAME}"
+			;;
+	esac
+	echo -e "$BBAR"
+	
+	echo -e " -> title [Y]es or [n]o  "
+	read -p "   -> your choice: " CHOICE
+	case "$CHOICE" in 
+		n ) 	
+			;;
+		* ) echo -e " -> write the title  "
+			read -p "   -> your choice: " LABEL 
+			echo -e "set title \"${LABEL}\";" >> "${FILE_NAME}"
+			;;
+	esac
+	echo -e "$BBAR"
+	
+
+}
+export -f generate_conf
--- make_plot.sh
+++ make_plot.sh
@@ -2,12 +2,21 @@
 
 source "include/gnuplot.sh"
 
-file="newton.dat"
-y_label="sqrt(x)"
-x_label="x"
-xxrange="[0.2:20]"
-yyrange="[0:12]"
-title="sqrt with Newton"
-output="sqrt_00.pdf"
-what="	$file using  2:3 title 'sqrt' with lines "
-plot
+
+MAKE_PLOT_CREATOR_PATH=$(echo -e $(readlink `which make_plot`) | sed "s/make_plot.sh//")
+if [ -f $MAKE_PLOT_CREATOR_PATH ]
+then
+    MAKE_PLOT_CREATOR_PATH="./"
+fi
+
+generate_conf
+
+# file="newton.dat"
+# y_label="sqrt(x)"
+# x_label="x"
+# xxrange="[0.2:20]"
+# yyrange="[0:12]"
+# title="sqrt with Newton"
+# output="sqrt_00.pdf"
+# what="	$file using  2:3 title 'sqrt' with lines "
+# plot
