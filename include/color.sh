4b0d3e90d282ad9d7a8ee819bb8c93b31fce970c
ESeNonFossiIo <esenonfossiio@gmail.com>
2015-04-22 17:47:07 +0200

    color and path fixed

---
+++ include/color.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+CLOSE_COLOR="\033[0m"
+
+# Regular Colors
+BLACK_COLOR="\[\033[0;30m\]" # Black
+RED_COLOR="\033[0;31m" # Red
+GREEN_COLOR="\[\033[0;32m\]" # Green
+YELLOW_COLOR="\[\033[0;33m\]" # Yellow
+BLUE_COLOR="\[\033[0;34m\]" # Blue
+PURPLE_COLOR="\[\033[0;35m\]" # Purple
+CYAN_COLOR="\[\033[0;36m\]" # Cyan
+WHITE_COLOR="\[\033[0;37m\]" # White
+
+# High Intensty
+HIGH_BLACK_COLOR="\[\033[0;90m\]" # Black
+HIGH_RED_COLOR="\033[0;91m" # Red
+HIGH_GREEN_COLOR="\[\033[0;92m\]" # Green
+HIGH_YELLOW_COLOR="\[\033[0;93m\]" # Yellow
+HIGH_BLUE_COLOR="\[\033[0;94m\]" # Blue
+HIGH_PURPLE_COLOR="\[\033[0;95m\]" # Purple
+HIGH_CYAN_COLOR="\[\033[0;96m\]" # Cyan
+HIGH_WHITE_COLOR="\[\033[0;97m\]" # White
+
+# # Background
+# \[\033[40m\]" # Black
+# \[\033[41m\]" # Red
+# \[\033[42m\]" # Green
+# \[\033[43m\]" # Yellow
+# \[\033[44m\]" # Blue
+# \[\033[45m\]" # Purple
+# \[\033[46m\]" # Cyan
+# \[\033[47m\]" # White
+# 
+# # High Intensty backgrounds
+# \[\033[0;100m\]" # Black
+# \[\033[0;101m\]" # Red
+# \[\033[0;102m\]" # Green
+# \[\033[0;103m\]" # Yellow
+# \[\033[0;104m\]" # Blue
+# \[\033[10;95m\]" # Purple
+# \[\033[0;106m\]" # Cyan
+# \[\033[0;107m\]" # White
+# 
+# #Replace any leading leading 0; with 1; for bold colors
+# #Replace any leading 0; with 4; to underline
